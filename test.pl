b638072d8baedacf4086bb4bef4595b2f9e6cf32
Daniel Stenberg <daniel@haxx.se>
2023-04-02 00:53:43 +0200

    test: add all cmdlines from the README

--- test.pl
+++ test.pl
@@ -12,6 +12,14 @@ my @t = (
     "https://example.com/test/../moo|https://example.com/moo",
     "localhost --append path=moo|http://localhost/moo",
     "--set host=moo --set scheme=http|http://moo/",
+    "--url https://curl.se --set host=example.com|https://example.com/",
+    "--set host=example.com --set scheme=ftp|ftp://example.com/",
+    "--url https://curl.se/we/are.html --redirect here.html|https://curl.se/we/here.html",
+    "--url https://curl.se/we/../are.html --set port=8080|https://curl.se:8080/are.html",
+    "--url https://curl.se/we/are.html --get '{path}'|/we/are.html",
+    "--url https://curl.se/we/are.html --get '{port}'|443",
+    "--url https://curl.se/hello --append path=you|https://curl.se/hello/you",
+    "--url \"https://curl.se?name=hello\" --append query=search=string|https://curl.se/?name=hello&search=string",
 );
 
 for my $c (@t) {
