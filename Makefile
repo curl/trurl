curl/trurl

commit a7f9ca9c42c2cf29cc84becb38b56082898868d4

bmake-compatible makefile
Needed for easier porting on OpenBSD

Closes #141

Makefile
@@ -1,7 +1,8 @@
 TARGET = trurl
 OBJS = trurl.o
-LDLIBS = -lcurl
-CFLAGS := $(CFLAGS) -W -Wall -pedantic -g
+LDLIBS != curl-config --libs
+CFLAGS != curl-config --cflags
+CFLAGS += -W -Wall -pedantic -g
 MANUAL = trurl.1
 
 PREFIX ?= /usr/local
@@ -11,6 +12,7 @@ MANDIR ?= $(DESTDIR)$(PREFIX)/share/man/man1
 INSTALL = install
 
 $(TARGET): $(OBJS)
+	$(CC) $(OBJS) -o $(TARGET) $(LDLIBS) $(LDFLAGS)
 
 trurl.o:trurl.c version.h
 

commit 817750d68a65aa93d92c71342874cadb35954da3

Respect CFLAGS, CPPFLAGS in Makefile, fix LDFLAGS order
The implicit rule handled this correctly but a7f9ca9c42c2cf29cc84becb38b56082898868d4
did away with that.

Add CPPFLAGS (for the C preprocessor) and CFLAGS explicitly, and then shift
LDFLAGS earlier to be before the object list, which is needed for e.g. -Wl,--as-needed
to work correctly.

Fixes: a7f9ca9c42c2cf29cc84becb38b56082898868d4

Makefile
@@ -12,7 +12,7 @@ INSTALL ?= install
 PYTHON3 ?= python3
 
 $(TARGET): $(OBJS)
-	$(CC) $(OBJS) -o $(TARGET) $(LDLIBS) $(LDFLAGS)
+	$(CC) $(CPPFLAGS) $(CFLAGS) $(LDFLAGS) $(OBJS) -o $(TARGET) $(LDLIBS)
 
 trurl.o:trurl.c version.h
 