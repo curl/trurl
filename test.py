fa58fde5d96a40ccc01ab5479c050078e9f7f270
Emanuele Torre <torreemanuele6@gmail.com>
2023-04-19 09:06:03 +0200

    test.py+tests.json: store arguments in a list instead of a string
    Python's subprocess.run() expects a list of arguments, and handles
    cmd.exe quoting by itself.
    
    I replaced "cmdline" with an object named "input" that contains an array
    named "arguments" with the arguments to pass to trurl.
    
    I also updated tests.json to reflect the change with the following
    command:
    
    python -c 'import sys, shlex, json; j = json.load(open("tests.json",
    "r"))'$'\n''for k,v in enumerate(j): c = v["cmdline"]; del
    j[k]["cmdline"]; j[k]["input"] = {}; j[k]["input"]["arguments"] =
    shlex.split(c)'$'\njson.dump(j,sys.stdout);print()' | jq --indent 4 '.[]
    |= {input,expected}' > tmp.json && mv tests.json tmp.json

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from typing import Any
+from typing import Any, Optional
 import json
 import subprocess
 import sys
@@ -12,7 +12,6 @@ BASECMD = "./trurl"
 
 RED = "\033[91m"  # used to mark unsuccessful tests
 NOCOLOR = "\033[0m"
-TAB = "\x09"
 
 
 @dataclass
@@ -36,17 +35,18 @@ def stripDict(d):
 class TestCase:
     def __init__(self, testIndex, **testCase):
         self.testIndex = testIndex
-        self.cmdline = testCase["cmdline"]
+        self.arguments = testCase["input"]["arguments"]
         self.expected = stripDict(testCase["expected"])
         self.commandOutput: CommandOutput = None
         self.testPassed: bool = False
 
-    def runCommand(self, stdinkeyword: str):
-        # returns false if the command does not contain keyword
-        if self.cmdline.find(stdinkeyword) == -1:
+    def runCommand(self, cmdfilter: Optional[str]):
+        # Skip test if none of the arguments contain the keyword
+        if cmdfilter and all(cmdfilter not in arg for arg in self.arguments):
             return False
+
         output = subprocess.run(
-            shlex.split(f"{BASECMD} {self.cmdline}"),
+            [BASECMD] + self.arguments,
             capture_output=True,
         )
 
@@ -93,8 +93,8 @@ class TestCase:
         print()
 
     def printConcise(self):
-        o = f"{self.testIndex}: {'passed' if self.testPassed else 'failed'}{TAB}'{self.cmdline}'"
-        print(o)
+        result = 'passed' if self.testPassed else 'failed'
+        print(f"{self.testIndex}: {result}\t{shlex.join(self.arguments)}")
 
 
 def main():
@@ -103,7 +103,7 @@ def main():
         testIndexesToRun = []
 
     # if argv[1] exists and starts with int
-    stdinfilter = ""
+    cmdfilter = ""
     testIndexesToRun = list(range(len(allTests)))
 
     if len(sys.argv) > 1:
@@ -115,13 +115,13 @@ def main():
                 testIndexesToRun.append(int(caseIndex))
 
         else:
-            stdinfilter = sys.argv[1]
+            cmdfilter = sys.argv[1]
 
     numTestsPassed = 0
     for testIndex in testIndexesToRun:
         test = TestCase(testIndex, **allTests[testIndex])
 
-        if test.runCommand(stdinfilter):
+        if test.runCommand(cmdfilter):
             if test.test():  # passed
                 test.printConcise()
                 numTestsPassed += 1
