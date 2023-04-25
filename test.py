80ae0841e1c1befedfb42939bfb778d48a0c4407
Krishean Draconis <70195763+krishean@users.noreply.github.com>
2023-04-25 09:05:37 -0400

    Add support for skipping tests

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
-from typing import Any, Optional
-import json
-import subprocess
 import sys
+from os import getcwd, path
+import re
+import json
 import shlex
+from subprocess import PIPE, run
 from dataclasses import dataclass, asdict
+from typing import Any, Optional
+from packaging.version import parse as parse_version
 
-TESTFILE = "./tests.json"
-BASECMD = "./trurl"
+PROGNAME = "trurl"
+TESTFILE = "tests.json"
 VALGRINDTEST = "valgrind"
 VALGRINDARGS = ["--error-exitcode=1", "--leak-check=full", "-q"]
 
@@ -24,8 +27,9 @@ class CommandOutput:
 
 
 class TestCase:
-    def __init__(self, testIndex, **testCase):
+    def __init__(self, testIndex, baseCmd, **testCase):
         self.testIndex = testIndex
+        self.baseCmd = baseCmd
         self.arguments = testCase["input"]["arguments"]
         self.expected = testCase["expected"]
         self.commandOutput: CommandOutput = None
@@ -36,15 +40,15 @@ class TestCase:
         if cmdfilter and all(cmdfilter not in arg for arg in self.arguments):
             return False
 
-        cmd = [BASECMD]
+        cmd = [self.baseCmd]
         args = self.arguments
         if runWithValgrind:
             cmd = [VALGRINDTEST]
-            args = VALGRINDARGS + [BASECMD] + self.arguments
+            args = VALGRINDARGS + [self.baseCmd] + self.arguments
 
-        output = subprocess.run(
+        output = run(
             cmd + args,
-            capture_output=True,
+            stdout=PIPE, stderr=PIPE,
             encoding="utf-8"
         )
 
@@ -100,49 +104,106 @@ class TestCase:
         print(f"{self.testIndex}: {result}\t{shlex.join(self.arguments)}")
 
 
-def main():
-    with open(TESTFILE, "r") as file:
-        allTests = json.load(file)
-        testIndexesToRun = []
+def main(argc, argv):
+    ret = 0
+    baseDir = path.dirname(path.realpath(argv[0]))
+    # python on windows does not always seem to find the
+    # executable if it is in a different output directory than
+    # the python script, even if it is in the current working
+    # directory, using absolute paths to the executable and json
+    # file makes it reliably find the executable
+    baseCmd = path.join(getcwd(), PROGNAME)
+    # the .exe on the end is necessary when using absolute paths
+    if sys.platform == "win32" or sys.platform == "cygwin":
+        baseCmd += ".exe"
+    # check if the trurl executable exists
+    if path.isfile(baseCmd):
+        # get the run-time and build-time libcurl versions
+        output = run(
+            [baseCmd, "--version"],
+            stdout=PIPE, stderr=PIPE,
+            encoding="utf-8"
+        )
+        version_re = r' libcurl\/([\w\.\-]+) \[built\-with ([\w\.\-]+)\]$'
+        match = re.findall(version_re, output.stdout)
+        libcurl_runtime, libcurl_buildtime = match[0]
+        libcurl_runtime = parse_version(libcurl_runtime)
+        libcurl_buildtime = parse_version(libcurl_buildtime)
 
-    # if argv[1] exists and starts with int
-    cmdfilter = ""
-    testIndexesToRun = list(range(len(allTests)))
-    runWithValgrind = False
-
-    for arg in sys.argv[1:]:
-        if arg[0].isnumeric():
-            # run only test cases separated by ","
+        with open(path.join(baseDir, TESTFILE), "r") as file:
+            allTests = json.load(file)
             testIndexesToRun = []
 
-            for caseIndex in arg.split(","):
-                testIndexesToRun.append(int(caseIndex))
-        elif arg == "--with-valgrind":
-            runWithValgrind = True
+        # if argv[1] exists and starts with int
+        cmdfilter = ""
+        testIndexesToRun = list(range(len(allTests)))
+        runWithValgrind = False
+
+        if argc > 1:
+            for arg in argv[1:]:
+                if arg[0].isnumeric():
+                    # run only test cases separated by ","
+                    testIndexesToRun = []
+
+                    for caseIndex in arg.split(","):
+                        testIndexesToRun.append(int(caseIndex))
+                elif arg == "--with-valgrind":
+                    runWithValgrind = True
+                else:
+                    cmdfilter = argv[1]
+
+        numTestsFailed = 0
+        numTestsPassed = 0
+        numTestsSkipped = 0
+        for testIndex in testIndexesToRun:
+            # skip tests if the run-time libcurl version is too low
+            if "minruntime" in allTests[testIndex]:
+                minruntime = allTests[testIndex]["minruntime"]
+                minruntime = parse_version(minruntime)
+                if libcurl_runtime < minruntime:
+                    skipMessage = "libcurl run-time version is too low: "
+                    skipMessage += f"{libcurl_runtime} < {minruntime}"
+                    print(f"{testIndex + 1}: skipped\t{skipMessage}")
+                    numTestsSkipped += 1
+                    continue
+            # skip tests if the build-time libcurl version is too low
+            if "minbuildtime" in allTests[testIndex]:
+                minbuildtime = allTests[testIndex]["minbuildtime"]
+                minbuildtime = parse_version(minbuildtime)
+                if libcurl_buildtime < minbuildtime:
+                    skipMessage = "libcurl build-time version is too low: "
+                    skipMessage += f"{libcurl_buildtime} < {minbuildtime}"
+                    print(f"{testIndex + 1}: skipped\t{skipMessage}")
+                    numTestsSkipped += 1
+                    continue
+
+            test = TestCase(testIndex + 1, baseCmd, **allTests[testIndex])
+
+            if test.runCommand(cmdfilter, runWithValgrind):
+                if test.test():  # passed
+                    test.printConcise()
+                    numTestsPassed += 1
+
+                else:
+                    test.printVerbose()
+                    numTestsFailed += 1
+
+        # finally print the results to terminal
+        print("Finished:")
+        result = ", ".join([
+            f"Failed: {numTestsFailed}",
+            f"Passed: {numTestsPassed}",
+            f"Skipped: {numTestsSkipped}",
+            f"Total: {len(testIndexesToRun)}"
+        ])
+        if (numTestsFailed == 0):
+            print("Passed! - ", result)
         else:
-            cmdfilter = sys.argv[1]
-
-    numTestsPassed = 0
-    for testIndex in testIndexesToRun:
-        test = TestCase(testIndex, **allTests[testIndex])
-
-        if test.runCommand(cmdfilter, runWithValgrind):
-            if test.test():  # passed
-                test.printConcise()
-                numTestsPassed += 1
-
-            else:
-                test.printVerbose()
-
-    # finally print the results to terminal
-
-    run = print
-    if (numTestsPassed != len(testIndexesToRun)):
-        run = sys.exit
-
-    print("finished:")
-    run(f"{numTestsPassed}/{len(testIndexesToRun)} tests passed")
+            ret = f"Failed! - {result}"
+    else:
+        ret = f" error: File \"{baseCmd}\" not found!"
+    return ret
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main(len(sys.argv), sys.argv))
