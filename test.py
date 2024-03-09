a7b2242f34eeffc948a4791a85f7c6df84e60c50
Jacob Mealey <jacobmealey.maine@gmail.com>
2024-03-09 13:30:11 -0500

    ugly but maybe will work?

--- test.py
+++ test.py
@@ -94,14 +94,19 @@ class TestCase:
             cmd = [VALGRINDTEST]
             args = VALGRINDARGS + [self.baseCmd] + self.arguments
 
-        output = run(
-            cmd + args,
-            stdout=PIPE, stderr=PIPE,
-            encoding="utf-8",
-            shell=False,
-            input=self.stdin 
-        )
-
+        if self.stdin:
+            output = run(
+                cmd + args,
+                stdout=PIPE, stderr=PIPE,
+                encoding="utf-8",
+                input=self.stdin 
+            )
+        else:
+            output = run(
+                cmd + args,
+                stdout=PIPE, stderr=PIPE,
+                encoding="utf-8",
+            )
         if isinstance(self.expected["stdout"], list):
             # if we don't expect string, parse to json
             try:
